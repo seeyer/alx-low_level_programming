f2ff6ca43a6b2e142f183803ffdcb7b3b33dfd55
seeyer <seeyerefiok@gmail.com>
2022-03-10 14:42:12 -0800

    llp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- *
- * Return: Always 0 (Success)
+ * main -Entry point
+ * Return: Always 0 (Sucess)
  *
  */
-
-int main(void)
+int main(0)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
