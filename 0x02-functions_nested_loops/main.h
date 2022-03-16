4b19d01f8f8b56837884867b4488d0c6790c0c4b
seeyer <seeyerefiok@gmail.com>
2022-03-16 10:23:04 -0700

    function nested loop

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 void print_to_98(int n);
+void print_times_table(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
