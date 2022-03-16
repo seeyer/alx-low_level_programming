51f81b8e1ed397c410cd7121a122d736be3ed413
seeyer <seeyerefiok@gmail.com>
2022-03-16 10:50:37 -0700

    function nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,22 +11,17 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-
 		for (i = 0; i <= n; i++)
 		{
-
 			for (j = 0; j <= n; j++)
 			{
-
 				k = j * i;
 				if (j == 0)
 				{
-
 					_putchar(k + '0');
 				}
 				else if (k < 10 && j != 0)
 				{
-
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
