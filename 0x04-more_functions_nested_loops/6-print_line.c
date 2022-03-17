cc1f66a3e2b1915d0e758c9606ff8aa5f7131ad6
seeyer <seeyerefiok@gmail.com>
2022-03-17 14:26:01 -0700

    more function nested loop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_line - prints a line with the length being the input
  * @n: the length of the line
@@ -8,6 +7,7 @@
 void print_line(int n)
 {
 	int i;
+
 	for (i = 0; i < n; ++i)
 		_putchar('_');
 	_putchar('\n');
