c11313816eb7a7fa3cec2fb07b1acb3c73855946
seeyer <seeyerefiok@gmail.com>
2022-03-24 04:52:29 -0700

    pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,6 @@
  * @s: input string.
  * Return: the pointer to dest.
  */
-
 char *cap_string(char *s)
 {
 	int count = 0, i;
@@ -12,21 +11,19 @@ char *cap_string(char *s)
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
-
-					count++;
-					while (*(s + count) != '\0')
-					{
-						for (i = 0; i < 13; i++)
-						{
-							if (*(s + count) == sep_words[i])
-							{
-								if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-									*(s + (count + 1)) = *(s + (count + 1)) - 32;
-								break;
-							}
-						}
-						count++;
-					}
-				
-					return (s);
+	count++;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
 }
