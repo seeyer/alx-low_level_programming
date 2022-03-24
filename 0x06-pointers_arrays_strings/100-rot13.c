79e5f6fa91199648588216fa1bed8beadfa5011b
seeyer <seeyerefiok@gmail.com>
2022-03-24 07:59:10 -0700

    pointer

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,6 +4,7 @@
  * @s: input string.
  * Return: the pointer to dest.
  */
+
 char *rot13(char *s)
 {
 	int count = 0, i;
@@ -16,8 +17,8 @@ char *rot13(char *s)
 		{
 			if (*(s + count) == alphabet[i])
 			{
-				*(s + count) = rot13[i]
-					break
+				*(s + count) = rot13[i];
+					break;
 			}
 		}
 		count++;
