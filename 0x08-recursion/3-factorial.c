44b441eecaea19427bb4f33e0785be951c2aabec
seeyer <seeyerefiok@gmail.com>
2022-03-30 05:19:59 -0700

    c recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,10 +6,8 @@
  */
 int factorial(int n)
 {
-
-		if (n == )
-			return (1);
-		else if (n < 0)
-			return (-1);
-		return (n * factorial(n - 1));
-}
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+			return (n * factorial(n - 1));}
