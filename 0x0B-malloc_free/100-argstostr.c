2cc40a4c5c5b662f8b67c38e8514474eae41380d
seeyer <seeyerefiok@gmail.com>
2022-04-04 17:53:06 -0700

    A function that returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * argstostr - convert the params passed to the program to string
  * @ac: the argument count
@@ -12,10 +11,8 @@ char *argstostr(int ac, char **av)
 {
 	int ch = 0, i = 0, j = 0, k = 0;
 	char *s;
-
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
 	while (i < ac)
 	{
 		while (av[i][j])
