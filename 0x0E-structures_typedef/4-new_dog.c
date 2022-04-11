b1fee5689f1ee4dfb550cf68a82ac4532d205d11
seeyer <seeyerefiok@gmail.com>
2022-04-11 13:20:37 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -42,6 +42,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(cutie_dog);
 			return (NULL);
 		}
+	
 		cutie_dog->name = _strcpy(cutie_dog->name, name);
 		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
 		cutie_dog->age = age;
