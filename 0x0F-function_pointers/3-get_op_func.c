bf97ffb65108cf3a197ea1dfad42474bbe4d2748
seeyer <45285003+seeyer@users.noreply.github.com>
2022-04-13 22:39:33 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,32 +3,32 @@
 #include <string.h>
 
 /**
- * <<<<<<< HEAD
+<<<<<<< HEAD
  * get_op_func - ...
  * @s: ...
- * 
+ *
  * Return: ...
  */
 =======
-* get_op_func - ...
-* @s: ...
-*
-* Return: ...
-*/
-	>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{ "+", op_add },
-		{ "-", op_sub },
-		{ "*", op_mul },
-		<<<<<<< HEAD
-		{ "/", op_div }
-		=======
-		{ "/", op_div },
-			>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-			{ "%", op_mod },
-			{ NULL, NULL }
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+<<<<<<< HEAD
+	{ "/", op_div }
+=======
+	{ "/", op_div },
+>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
 	int i = 0;
 
@@ -36,12 +36,12 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-		<<<<<<< HEAD
+<<<<<<< HEAD
 		
-			=======
-		
-			>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-			i++;
+=======
+
+>>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+		i++;
 	}
 
 	return (0);
