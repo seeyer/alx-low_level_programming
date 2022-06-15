560f0f019dabd01e0fc8aae4c9229b99491ee406
seeyer <seeyerefiok@gmail.com>
2022-06-15 12:39:35 -0700

    my second commit

---
+++ 0x18-dynamic_libraries/100-operation.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+int add (int a, int b)
+{
+	return a + b;
+}
+
+int sub(int a, int b )
+{
+	return a - b;
+}
+
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+int div(int a, int b)
+{
+	return a / b;
+}
+
+int mods(int a, int b)
+{
+	return a % b;
+}
